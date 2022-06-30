4961c5a465913f901a5b34870ecc5dbb23816be9
Vladislav <porvinvlad.2019@gmail.com>
2022-07-01 01:27:13 +0300

    last version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 window.addEventListener('DOMContentLoaded', () => {
   // Slider
   $(function () {
@@ -13,19 +14,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // The main part of JS
 
-  const articlesContent = document.querySelector('.articles__content');
-  const select = document.querySelector('select');
-  const articlesSearch = document.querySelector('.articles__search');
-
-  const pricesFrom = document.querySelector('#prices__from');
-  const pricesTo = document.querySelector('#prices__to');
-  pricesFrom.addEventListener('change', () => {
-    console.log(pricesFrom.value);
-  });
+  const articlesContent = document.querySelector('.articles__content'),
+    select = document.querySelector('select'),
+    articlesSearch = document.querySelector('.articles__search'),
+    datesFrom = document.querySelector('#dates__from'),
+    datesTo = document.querySelector('#dates__to');
 
   function getDatesInRange(startDate, endDate) {
     const date = new Date(startDate.getTime());
-
     const dates = [];
 
     while (date <= endDate) {
@@ -35,16 +31,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     return dates;
   }
-
+  // С указанного API посты датируются одной датой (те что  я брал), поэтому добавлял рандомные месяцы, чтобы имитировать разные даты
   const getRandomMonth = () => {
     return Math.floor(Math.random() * 100);
   };
+  // Проверка дат на совпадение
   const datesAreSame = (first, second) =>
     first.getFullYear() === second.getFullYear() &&
     first.getMonth() === second.getMonth() &&
     first.getDate() === second.getDate();
-
-  const insertTitles = (parent, data, i) => {
+  // Вставка статей
+  const insertArticles = (parent, data, i) => {
     parent.innerHTML += `
     <div class='articles__content-item'>
         <p class='articles__content-date'>${new Date(data[i].publishedAt).toLocaleString('ru', {
@@ -69,35 +66,37 @@ window.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < 6; i++) {
           articles[i].publishedAt = new Date(articles[i].publishedAt);
           articles[i].publishedAt.setDate(articles[i].publishedAt.getDate() + getRandomMonth());
-          insertTitles(articlesContent, articles, i);
+          insertArticles(articlesContent, articles, i);
           select.innerHTML += `
           <option class="search-name__item"  value='${articles[i].author.toLowerCase()}'>
             ${articles[i].author}
           </option>`;
         }
+        // Selecting author
         select.addEventListener('click', (event) => {
           const value = event.target.value;
           articlesContent.innerHTML = ``;
           for (let i = 0; i < 6; i++) {
             if (value == 'init') {
-              insertTitles(articlesContent, articles, i);
+              insertArticles(articlesContent, articles, i);
             } else if (articles[i].author.toLowerCase() == value) {
-              insertTitles(articlesContent, articles, i);
+              insertArticles(articlesContent, articles, i);
             }
           }
         });
 
-        pricesFrom.addEventListener('change', () => {
-          const d1 = new Date(pricesFrom.value);
-          pricesTo.addEventListener('change', () => {
+        // Searching articles with the same dates
+        datesFrom.addEventListener('change', () => {
+          const d1 = new Date(datesFrom.value);
+          datesTo.addEventListener('change', () => {
             articlesContent.innerHTML = ``;
-            const d2 = new Date(pricesTo.value);
+            const d2 = new Date(datesTo.value);
             let datesArr = getDatesInRange(d1, d2);
             for (let i = 0; i < datesArr.length - 1; i++) {
               for (let j = 0; j < 6; j++) {
                 if (datesAreSame(datesArr[i], articles[j].publishedAt)) {
                   console.log(datesAreSame(datesArr[i], articles[j].publishedAt));
-                  insertTitles(articlesContent, articles, j);
+                  insertArticles(articlesContent, articles, j);
                 }
               }
             }
@@ -127,4 +126,16 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   getData();
+});
+// Добавление картинки телефона и почты при ресайзе контента в браузере
+window.addEventListener('resize', () => {
+  (contactsTelLink = document.querySelector('.contacts__tel-link')),
+    (contactsMailLink = document.querySelector('.contacts__email-link'));
+  if (window.innerWidth <= 505) {
+    contactsTelLink.innerHTML = `<img src='../images/phone.svg' />`;
+    contactsMailLink.innerHTML = `<img src='../images/mail.svg' />`;
+  } else {
+    contactsTelLink.innerHTML = `8 800 000 00 00`;
+    contactsMailLink.innerHTML = `sales@logo.ru`;
+  }
 });
